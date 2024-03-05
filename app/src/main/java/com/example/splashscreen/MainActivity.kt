79987e1ad4006d9a568dd2f5f66a68206dd02cc8
vikashkumaraaryan <161560975+vikashkumaraaryan@users.noreply.github.com>
2024-03-05 11:00:34 +0530

    tap time change text
    when i tap on text after change text.

--- app/src/main/java/com/example/splashscreen/MainActivity.kt
+++ app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.splashscreen
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left , systemBars.top , systemBars.right , systemBars.bottom)
             insets
         }
+        var a=findViewById<Button>(R.id.butt)
+            a.setOnClickListener {
+              a.setText("vikash")
+                Toast.makeText(this , "changed text" , Toast.LENGTH_SHORT).show()
+            }
+        
     }
 }
